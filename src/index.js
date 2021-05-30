fa149d3a9a2d5c3bdabf66a5bff93116046cc95c
cjabeyakaran <cjabeyakaran@wustl.edu>
2021-05-29 22:36:42 -0400

    barebones react app

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import $ from 'jquery';
+import './index.css';
+import App from './App'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  $("#root")
+);
