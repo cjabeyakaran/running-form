060dd7a546267101d3c0629859a979bf1b32eef1
cjabeyakaran <cjabeyakaran@wustl.edu>
2021-06-25 17:51:33 -0500

    Completed mid stance analysis

--- src/components/PoseCard.js
+++ src/components/PoseCard.js
@@ -41,7 +41,6 @@ class PoseCard extends React.PureComponent {
         const poses = await net.estimateMultiplePoses(image);
         console.log(poses);
 
-        // this.setState({keypoints: poses[0].keypoints});
         return poses[0];
     }
 
@@ -107,8 +106,10 @@ class PoseCard extends React.PureComponent {
         let ifOverstriding = (angleLegTorso > 0.18) || (angleLegGround > 1.5);
         if (! ifOverstriding) {
             this.setState({analysis: "noissue"});
+            return;
         } else {
             this.setState({analysis: "overstriding"});
+            return;
         }
     }
 
@@ -126,8 +127,86 @@ class PoseCard extends React.PureComponent {
         ctx.stroke();
     }
 
-    analyzeMid(id) {
+    analyzeMid(keypoints) {
+        let leftAnkle = keypoints[15].position;
+        let rightAnkle = keypoints[16].position;
+        let ankle, knee;
 
+        // check which foot is landing
+        if (leftAnkle.y > rightAnkle.y) {
+            ankle = leftAnkle;
+            knee = keypoints[13].position;
+        } else {
+            ankle = rightAnkle;
+            knee = keypoints[14].position;
+        }
+        
+        let nose = keypoints[0].position;
+        let ear, shoulder, elbow, hip;
+        
+        let isLeft = keypoints[3].score > keypoints[4].score; // tells us if runner is facing left or right
+        if (isLeft) {
+            ear = keypoints[3].position;
+            shoulder = keypoints[5].position;
+            elbow = keypoints[7].position;
+            hip = keypoints[11].position;
+
+            if (nose.x < ear.x) {
+                this.setState({analysis: "limp", keypoints: {nose: nose, knee: knee}});
+                return;
+            }
+        } else {
+            ear = keypoints[4].position;
+            shoulder = keypoints[6].position;
+            elbow = keypoints[8].position;
+            hip = keypoints[12].position;
+
+            if (nose.x > ear.x) {
+                this.setState({analysis: "limp", keypoints: {nose: nose, knee: knee}});
+                return;
+            }
+        }
+
+        let legVector = {dx: knee.x - ankle.x, dy: knee.y - ankle.y};
+        let torsoVector = {dx: shoulder.x - hip.x, dy: shoulder.y - hip.y};
+
+        let angleLegTorso = this.calculateVectorAngle(legVector, torsoVector);
+
+        if (angleLegTorso > 0.1) { // if leg and torso form a "V" then locked posture
+            this.setState({analysis: "locked", keypoints: {shoulder: shoulder, hip: hip, knee: knee, ankle: ankle}});
+            return;
+        } else {
+            this.setState({analysis: "noissue", keypoints: {ear: ear, shoulder: shoulder, elbow: elbow, knee: knee, ankle: ankle}});
+        }
+    }
+
+    drawMid(id) {
+        let ctx = document.getElementById(id).getContext("2d");
+        ctx.strokeStyle = (this.state.analysis === "noissue") ? "#00FF00" : "#FF0000";
+        
+        if (this.state.analysis === "limp") {
+            // draw vertical line from knee
+            ctx.beginPath();
+            ctx.moveTo(this.state.keypoints.knee.x, this.state.keypoints.knee.y + 20);
+            ctx.lineTo(this.state.keypoints.knee.x, this.state.keypoints.knee.y - 200);
+            ctx.stroke();
+        } else if (this.state.analysis === "locked") {
+            // draw leg and torso vector
+            ctx.beginPath();
+            ctx.moveTo(this.state.keypoints.knee.x, this.state.keypoints.knee.y);
+            ctx.lineTo(this.state.keypoints.ankle.x, this.state.keypoints.ankle.y);
+            ctx.stroke();
+
+            ctx.moveTo(this.state.keypoints.shoulder.x, this.state.keypoints.shoulder.y);
+            ctx.lineTo(this.state.keypoints.hip.x, this.state.keypoints.hip.y);
+            ctx.stroke();
+        } else if (this.state.analysis === "noissue") {
+            // draw arm vector through ear and leg vector
+            ctx.beginPath();
+            ctx.moveTo(this.state.keypoints.ear.x, this.state.keypoints.ear.y);
+            ctx.lineTo(this.state.keypoints.shoulder.x, this.state.keypoints.shoulder.y);
+            ctx.lineTo(this.state.keypoints.elbow.x, this.state.keypoints.elbow.y);
+        }
     }
 
     analyzePushOff(id) {
@@ -138,9 +217,9 @@ class PoseCard extends React.PureComponent {
         let keypoints = pose.keypoints;
         if (this.props.stage === "iff") {
             this.analyzeIFF(keypoints);
-        } // else if (this.props.stage === "mid") {
-        //     this.analyzeMid(id);
-        // } else if (this.props.stage === "push") {
+        } else if (this.props.stage === "mid") {
+            this.analyzeMid(keypoints);
+        } // else if (this.props.stage === "push") {
         //     this.analyzePushOff(id);
         // }
     }
@@ -148,9 +227,9 @@ class PoseCard extends React.PureComponent {
     draw(id) {
         if (this.props.stage === "iff") {
             this.drawIff(id);
-        } // else if (this.props.stage === "mid") {
-        //     this.analyzeMid(id);
-        // } else if (this.props.stage === "push") {
+        } else if (this.props.stage === "mid") {
+            this.drawMid(id);
+        } // else if (this.props.stage === "push") {
         //     this.analyzePushOff(id);
         // }
     }
@@ -171,7 +250,7 @@ class PoseCard extends React.PureComponent {
             }, () => {
                 console.log("Loaded from firestore");
                 this.draw(canvasId);
-            }); // call appropriate draw function for the specific stance
+            });
         }
     }
 
