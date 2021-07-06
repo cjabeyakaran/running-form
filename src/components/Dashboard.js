e22e7a93d39ac18644060ef95e208c0cc273cb20
cjabeyakaran <cjabeyakaran@wustl.edu>
2021-07-06 15:00:27 -0500

    Rearrange app bar

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -5,16 +5,30 @@ import {
 	Button,
 	Typography
 } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import { Alert } from '@material-ui/lab';
 import { useHistory } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 import '../css/Dashboard.css';
 import CardCollection from './CardCollection'
 
+const useStyles = makeStyles((theme) => ({
+	root: { flexGrow: 1 },
+	title: { 
+		edge: "start",
+		flexGrow: 1 
+	},
+	button: {
+		edge: "end",
+		backgroundColor: theme.palette.secondary.light 
+	}
+}));
+
 function Dashboard(props) {
 	const [error, setError] = useState("");
 	const { currentUser, logout } = useAuth();
 	const history = useHistory();
+	const classes = useStyles();
 
 	const handlelogout = async () => {
 		try {
@@ -27,17 +41,17 @@ function Dashboard(props) {
 
 	return (
 		<>
-			<AppBar position="static">
-				<Toolbar>
-				<Typography variant="h6">
-					{currentUser.displayName}
-				</Typography>
-				<div className="logout">
+			<div className={classes.root}>
+				<AppBar position="static">
+					<Toolbar>
+					<Typography variant="h6" className={classes.title}>
+						{currentUser.displayName}
+					</Typography>
 					{error && <Alert severity="error"> {error} </Alert>}
-					<Button onClick={handlelogout}> Log Out </Button>
-				</div>
-				</Toolbar>
-			</AppBar>
+					<Button onClick={handlelogout} className={classes.button} > Log Out </Button>
+					</Toolbar>
+				</AppBar>
+			</div>
 			<CardCollection user={currentUser.uid}/>
 		</>
 	);
